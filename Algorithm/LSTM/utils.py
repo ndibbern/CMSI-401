52642cbfdcc4e78eea1b7e7ce324d58feadcae5f
Natalia Dibbern <natidibbern@gmail.com>
2018-11-12 18:23:01 -0800

    chore: Implement final version of LSTM algorithm using all input features

---
+++ Algorithm/LSTM/utils.py
@@ -0,0 +1,195 @@
+import torch
+import numpy as np
+
+def get_error( scores , labels ):
+
+    bs=scores.size(0)
+    predicted_labels = scores.detach().numpy()
+    array_labels = labels.detach().numpy()
+    num_matches = 0
+    for i in range(len(predicted_labels[0])):
+        if abs(predicted_labels[0][i] - array_labels[i]) < 0.01:
+            num_matches += 1
+    # indicator = (abs(predicted_labels.item() - labels.item()) < 0.001)
+    # num_matches = indicator.sum()
+    # print(num_matches)
+
+    return 1-num_matches/len(array_labels)
+
+
+def show(X):
+    if X.dim() == 3 and X.size(0) == 3:
+        plt.imshow( np.transpose(  X.numpy() , (1, 2, 0))  )
+        plt.show()
+    elif X.dim() == 2:
+        plt.imshow(   X.numpy() , cmap='gray'  )
+        plt.show()
+    else:
+        print('WRONG TENSOR SIZE')
+
+def show_prob_cifar(p):
+
+
+    p=p.data.squeeze().numpy()
+
+    ft=15
+    label = ('airplane', 'automobile', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship','Truck' )
+    #p=p.data.squeeze().numpy()
+    y_pos = np.arange(len(p))*1.2
+    target=2
+    width=0.9
+    col= 'blue'
+    #col='darkgreen'
+
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+
+    # the plot
+    ax.barh(y_pos, p, width , align='center', color=col)
+
+    ax.set_xlim([0, 1.3])
+    #ax.set_ylim([-0.8, len(p)*1.2-1+0.8])
+
+    # y label
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(label, fontsize=ft)
+    ax.invert_yaxis()
+    #ax.set_xlabel('Performance')
+    #ax.set_title('How fast do you want to go today?')
+
+    # x label
+    ax.set_xticklabels([])
+    ax.set_xticks([])
+    #x_pos=np.array([0, 0.25 , 0.5 , 0.75 , 1])
+    #ax.set_xticks(x_pos)
+    #ax.set_xticklabels( [0, 0.25 , 0.5 , 0.75 , 1] , fontsize=15)
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_linewidth(4)
+
+
+    for i in range(len(p)):
+        str_nb="{0:.2f}".format(p[i])
+        ax.text( p[i] + 0.05 , y_pos[i] ,str_nb ,
+                 horizontalalignment='left', verticalalignment='center',
+                 transform=ax.transData, color= col,fontsize=ft)
+
+
+
+    plt.show()
+
+
+
+def show_prob_mnist(p):
+
+    p=p.data.squeeze().numpy()
+
+    ft=15
+    label = ('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight','nine')
+    #p=p.data.squeeze().numpy()
+    y_pos = np.arange(len(p))*1.2
+    target=2
+    width=0.9
+    col= 'blue'
+    #col='darkgreen'
+
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+
+    # the plot
+    ax.barh(y_pos, p, width , align='center', color=col)
+
+    ax.set_xlim([0, 1.3])
+    #ax.set_ylim([-0.8, len(p)*1.2-1+0.8])
+
+    # y label
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(label, fontsize=ft)
+    ax.invert_yaxis()
+    #ax.set_xlabel('Performance')
+    #ax.set_title('How fast do you want to go today?')
+
+    # x label
+    ax.set_xticklabels([])
+    ax.set_xticks([])
+    #x_pos=np.array([0, 0.25 , 0.5 , 0.75 , 1])
+    #ax.set_xticks(x_pos)
+    #ax.set_xticklabels( [0, 0.25 , 0.5 , 0.75 , 1] , fontsize=15)
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_linewidth(4)
+
+
+    for i in range(len(p)):
+        str_nb="{0:.2f}".format(p[i])
+        ax.text( p[i] + 0.05 , y_pos[i] ,str_nb ,
+                 horizontalalignment='left', verticalalignment='center',
+                 transform=ax.transData, color= col,fontsize=ft)
+
+
+
+    plt.show()
+    #fig.savefig('pic/prob', dpi=96, bbox_inches="tight")
+
+
+
+
+
+
+def show_prob_fashion_mnist(p):
+
+
+    p=p.data.squeeze().numpy()
+
+    ft=15
+    label = ('T-shirt', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag','Boot')
+    #p=p.data.squeeze().numpy()
+    y_pos = np.arange(len(p))*1.2
+    target=2
+    width=0.9
+    col= 'blue'
+    #col='darkgreen'
+
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+
+    # the plot
+    ax.barh(y_pos, p, width , align='center', color=col)
+
+    ax.set_xlim([0, 1.3])
+    #ax.set_ylim([-0.8, len(p)*1.2-1+0.8])
+
+    # y label
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(label, fontsize=ft)
+    ax.invert_yaxis()
+    #ax.set_xlabel('Performance')
+    #ax.set_title('How fast do you want to go today?')
+
+    # x label
+    ax.set_xticklabels([])
+    ax.set_xticks([])
+    #x_pos=np.array([0, 0.25 , 0.5 , 0.75 , 1])
+    #ax.set_xticks(x_pos)
+    #ax.set_xticklabels( [0, 0.25 , 0.5 , 0.75 , 1] , fontsize=15)
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_linewidth(4)
+
+
+    for i in range(len(p)):
+        str_nb="{0:.2f}".format(p[i])
+        ax.text( p[i] + 0.05 , y_pos[i] ,str_nb ,
+                 horizontalalignment='left', verticalalignment='center',
+                 transform=ax.transData, color= col,fontsize=ft)
+
+
+
+    plt.show()
+    #fig.savefig('pic/prob', dpi=96, bbox_inches="tight")
